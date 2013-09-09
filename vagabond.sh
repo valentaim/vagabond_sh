6b5cd460dd1f8f9b73ca1f62479033d652d570f2
Valentaim <fokinvs@gmail.com>
2013-09-09 11:29:36 +0400

    Remove install git

--- vagabond.sh
+++ vagabond.sh
@@ -1,6 +1,5 @@
 apt-get install -y ruby1.9.3
 apt-get install -y make
-apt-get install -y git
 export VAGA_DIR="${HOME}/work/vagabond" && mkdir -p $VAGA_DIR && cd $VAGA_DIR
 git clone https://github.com/omgbebebe/vagabond.git
 cd vagabond
